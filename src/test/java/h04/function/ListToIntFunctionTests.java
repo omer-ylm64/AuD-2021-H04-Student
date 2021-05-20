d07b523d1e00908c5dfd80cc36797206e8ff9fec
Daniel Mangold <daniel.mangold@stud.tu-darmstadt.de>
2021-05-20 12:47:09 +0200

    Added tests for FunctionOnRatioOfInversions

--- src/test/java/h04/function/ListToIntFunctionTests.java
+++ src/test/java/h04/function/ListToIntFunctionTests.java
@@ -1,5 +1,6 @@
 package h04.function;
 
+import h04.provider.RatioOfInversionsProvider;
 import h04.provider.RatioOfRunsProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -277,6 +278,91 @@ class FunctionOnRatioOfRunsTest {
         @ArgumentsSource(RatioOfRunsProvider.class)
         public void testValidArguments(List<Integer> sequence, int numberOfRuns) throws ReflectiveOperationException {
             assertEquals((int) ((double) numberOfRuns / sequence.size()), apply.invoke(instance, sequence));
+        }
+    }
+}
+
+@DefinitionCheck("checkClass")
+class FunctionOnRatioOfInversionsTest {
+
+    public static Class<?> functionOnRatioOfInversionsClass;
+    public static Constructor<?> constructor;
+    public static Field doubleToIntFunction;
+    public static Method apply;
+
+    private static Object instance;
+    private static Object doubleToIntFunctionProxy;
+
+    @BeforeAll
+    public static void checkClass() throws ReflectiveOperationException {
+        assumeTrue(definitionCorrect(FunctionOnDegreeOfDisorderTest.class),
+                "FunctionOnRatioOfInversionsTest requires FunctionOnDegreeOfDisorder to be implemented correctly");
+        assumeTrue(definitionCorrect(DoubleToIntFunctionTest.class),
+                "FunctionOnRatioOfInversionsTest requires DoubleToIntFunction to be implemented correctly");
+
+        functionOnRatioOfInversionsClass = getClassForName("h04.function.FunctionOnRatioOfInversions");
+
+        // is public
+        assertTrue(isPublic(functionOnRatioOfInversionsClass.getModifiers()));
+
+        // is generic
+        assertEquals(1, functionOnRatioOfInversionsClass.getTypeParameters().length, "FunctionOnRatioOfInversions must be generic");
+        assertEquals("T", functionOnRatioOfInversionsClass.getTypeParameters()[0].getName(), "Type parameter is not named 'T'");
+
+        // extends FunctionOnDegreeOfDisorder<T>
+        assertEquals("h04.function.FunctionOnDegreeOfDisorder<T>", functionOnRatioOfInversionsClass.getGenericSuperclass().getTypeName(),
+                "FunctionOnRatioOfInversions must extend FunctionOnDegreeOfDisorder");
+
+        // is not abstract
+        assertFalse(isAbstract(functionOnRatioOfInversionsClass.getModifiers()), "FunctionOnRatioOfInversions must not be abstract");
+
+        // constructors
+        try {
+            constructor = functionOnRatioOfInversionsClass.getDeclaredConstructor(DoubleToIntFunctionTest.doubleToIntFunctionClass, Comparator.class);
+        } catch (NoSuchMethodException e) {
+            fail("FunctionOnRatioOfInversions is missing a required constructor", e);
+        }
+
+        assertTrue(isPublic(constructor.getModifiers()), "Constructor of FunctionOnRatioOfInversions must be public");
+        assertEquals("java.util.Comparator<? super T>", constructor.getGenericParameterTypes()[1].getTypeName(),
+                "Second parameter of constructor of FunctionOnDegreeOfDisorder is not parameterized correctly");
+
+        // fields
+        for (Field field : functionOnRatioOfInversionsClass.getDeclaredFields())
+            if (isPrivate(field.getModifiers()) &&
+                    isFinal(field.getModifiers()) &&
+                    field.getType().equals(DoubleToIntFunctionTest.doubleToIntFunctionClass))
+                doubleToIntFunction = field;
+
+        assertNotNull(doubleToIntFunction, "FunctionOnDegreeOfDisorder has no field matching the criteria specified by the assignment");
+
+        doubleToIntFunction.setAccessible(true);
+
+        // methods
+        apply = functionOnRatioOfInversionsClass.getDeclaredMethod("apply", List.class);
+
+
+        doubleToIntFunctionProxy = doubleToIntFunctionProxy();
+        instance = constructor.newInstance(doubleToIntFunctionProxy, Comparator.naturalOrder());
+    }
+
+    @Test
+    public void testDoubleToIntFunctionField() throws ReflectiveOperationException {
+        assertSame(doubleToIntFunctionProxy, doubleToIntFunction.get(instance), "Field of type DoubleToIntFunction is not set correctly");
+    }
+
+    @Nested
+    class ApplyTests {
+
+        @Test
+        public void testIllegalArguments() {
+            assertThrows(NullPointerException.class, () -> getActualException(apply, instance, (Object) null));
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(RatioOfInversionsProvider.class)
+        public void testValidArguments(List<Integer> sequence, int numberOfInversions, int maxNumberOfInversions) throws ReflectiveOperationException {
+            assertEquals((int) ((double) numberOfInversions / maxNumberOfInversions), apply.invoke(instance, sequence));
         }
     }
 }
