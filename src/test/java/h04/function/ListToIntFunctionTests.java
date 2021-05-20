18a10098a928da6fa5cd991503e5ae0912bb10cf
Daniel Mangold <daniel.mangold@stud.tu-darmstadt.de>
2021-05-20 08:50:11 +0200

    Added tests for FunctionOnDegreeOfDisorder

--- src/test/java/h04/function/ListToIntFunctionTests.java
+++ src/test/java/h04/function/ListToIntFunctionTests.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import static h04.Utils.*;
@@ -133,4 +134,61 @@ class ConstantListToIntFunctionTest {
             assertEquals(intField.get(instance), apply.invoke(instance, List.of(1, 2, 3, 4, 5)));
         }
     }
+}
+
+@DefinitionCheck("checkClass")
+class FunctionOnDegreeOfDisorderTest {
+
+    public static Class<?> functionOnDegreeOfDisorderClass;
+    public static Field comparator;
+
+    @BeforeAll
+    public static void checkClass() {
+        assumeTrue(definitionCorrect(ListToIntFunctionTest.class),
+                "FunctionOnDegreeOfDisorderTest requires ListToIntFunction to be implemented correctly");
+
+        functionOnDegreeOfDisorderClass = getClassForName("h04.function.FunctionOnDegreeOfDisorder");
+
+        // is public
+        assertTrue(isPublic(functionOnDegreeOfDisorderClass.getModifiers()));
+
+        // is generic
+        assertEquals(1, functionOnDegreeOfDisorderClass.getTypeParameters().length, "FunctionOnDegreeOfDisorder must be generic");
+        assertEquals("T", functionOnDegreeOfDisorderClass.getTypeParameters()[0].getName(), "Type parameter is not named 'T'");
+
+        // implements ListToIntFunction<T>
+        assertTrue(Arrays.stream(functionOnDegreeOfDisorderClass.getGenericInterfaces())
+                        .anyMatch(type -> type.getTypeName().equals("h04.function.ListToIntFunction<T>")),
+                "FunctionOnDegreeOfDisorder must implement ListToIntFunction<T>");
+
+        // is abstract
+        assertTrue(isAbstract(functionOnDegreeOfDisorderClass.getModifiers()), "FunctionOnDegreeOfDisorder must be abstract");
+
+        // constructors
+        Constructor<?> constructor = null;
+
+        try {
+            constructor = functionOnDegreeOfDisorderClass.getDeclaredConstructor(Comparator.class);
+        } catch (NoSuchMethodException e) {
+            fail("FunctionOnDegreeOfDisorder is missing a required constructor", e);
+        }
+
+        assertTrue(isPublic(constructor.getModifiers()), "Constructor of FunctionOnDegreeOfDisorder must be public");
+        assertEquals("java.util.Comparator<? super T>", constructor.getGenericParameterTypes()[0].getTypeName(),
+                "First parameter of constructor of FunctionOnDegreeOfDisorder is not parameterized correctly");
+
+        // fields
+        for (Field field : functionOnDegreeOfDisorderClass.getDeclaredFields())
+            if (isProtected(field.getModifiers()) &&
+                    isFinal(field.getModifiers()) &&
+                    field.getGenericType().getTypeName().equals("java.util.Comparator<? super T>"))
+                comparator = field;
+
+        assertNotNull(comparator, "FunctionOnDegreeOfDisorder has no field matching the criteria specified by the assignment");
+
+        comparator.setAccessible(true);
+    }
+
+    @Test
+    public void classDefinitionCorrect() {}
 }
