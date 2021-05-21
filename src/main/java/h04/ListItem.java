81c16b6d6098c47314b7435775640481ccc37b5b
Alexander Staeding <alexanderstaeding@gmail.com>
2021-05-21 11:32:48 +0200

    Add ListItem to template

---
+++ src/main/java/h04/ListItem.java
@@ -0,0 +1,13 @@
+package h04;
+
+public class ListItem<T> {
+  /**
+   * Public no-args constructor DO NOT DELETE
+   */
+  public ListItem() {
+  }
+
+  // DO NOT RENAME THESE
+  public T key;
+  public ListItem<T> next;
+}
